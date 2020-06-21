aecb6a6fc05b97777c084f05b8cc7cfe5d8ace4a
Ali Bayramli <alibayramli97@gmail.com>
2020-06-21 23:59:13 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fun-factory/precache-manifest.815035982332530e672152ee375cff52.js"
+  "/fun-factory/precache-manifest.803254b83fb8dbcde89f9050c115307f.js"
 );
 
 self.addEventListener('message', (event) => {
