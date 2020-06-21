600e34eec803ce1ac9265c41db7c541eb9de806e
Ali Bayramli <alibayramli97@gmail.com>
2020-06-21 21:05:02 +0200

    Change appbar color

--- src/projectComponents/Navbar.js
+++ src/projectComponents/Navbar.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "./Navbar.css";
+import { makeStyles } from "@material-ui/core/styles";
 import { AppBar, Typography, Toolbar, IconButton } from "@material-ui/core/";
+const useStyles = makeStyles(theme => ({
+  root: {
+    boxShadow: "none",
+    backgroundColor: "purple"
+  }
+}));
 export default function Navbar() {
+
+  const classes = useStyles();
   return (
-    <AppBar position="static">
+    <AppBar position="static" className={classes.root}>
       <Toolbar variant="dense">
         <IconButton edge="start" color="inherit" aria-label="menu"></IconButton>
         <Typography variant="h4" color="inherit">
