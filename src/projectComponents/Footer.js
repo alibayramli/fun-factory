31a127ff7cd0912bde95da470476493efb8939c6
Ali Bayramli <alibayramli97@gmail.com>
2020-06-21 16:22:57 +0200

    Update footer

--- src/projectComponents/Footer.js
+++ src/projectComponents/Footer.js
@@ -1,24 +1,67 @@
-import React from "react";
-import Typography from "@material-ui/core/Typography";
-import Link from "@material-ui/core/Link";
-import { makeStyles } from "@material-ui/core/styles";
-const useStyles = makeStyles(theme => ({
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary">
+      {`© ${new Date().getFullYear()} `}
+      <Link color="inherit" href="https://github.com/alibayramli">
+        Ali Bayramli
+      </Link>{' '}
+
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '50vh',
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
   footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6)
-  }
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+  },
 }));
+
 export default function Footer() {
   const classes = useStyles();
+
   return (
-    <footer className={classes.footer}>
-      <Typography variant="subtitle1" align="center" color="textSecondary">
-        {" © "}
-        {new Date().getFullYear() + " "}
-        <Link color="inherit" href="#">
-          Ali Bayramli
-        </Link>{" "}
-      </Typography>
-    </footer>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Container component="main" className={classes.main} maxWidth="sm">
+        <Typography variant="h2" component="h1" gutterBottom>
+          Source code
+        </Typography>
+        <Typography
+          variant="h5"
+          align="center"
+          color="textSecondary"
+          paragraph
+        >
+          You can see the source code in my <a href={'https://github.com/alibayramli/fun-factory'} style={{ textDecoration: 'none' }}>GitHub</a>
+        </Typography>
+
+      </Container>
+
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+
+          <Copyright />
+        </Container>
+      </footer>
+    </div>
   );
 }
